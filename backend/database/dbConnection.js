5acc5159e6d811a461c2ad07e01074991540aaf3
MSI <hirun08072002@gmail.com>
2024-10-23 21:37:28 +0530

    complete the dbconnection file without database name

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -4,5 +4,9 @@ import  Mongoose  from "mongoose";
 export const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URI,{
         dbName: ""
+    }).then(() => {
+        console.log("Connected to database")
+    }).catch((err) => {
+        console.log(`some error occured while connecting to database: ${err}`)
     })
 }
