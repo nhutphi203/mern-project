40e9b4c31298d04c41db91f77ace941baf023bbd
MSI <hirun08072002@gmail.com>
2024-10-23 17:09:30 +0530

    setup cors and other items

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,14 @@
 import express from "express";
-import {config} from "dotenv"
+import {config} from "dotenv";
+import cors from "cors";
 
 const app= express();
 config( {path: "./config/config.env"})
+
+app.use(cors({
+    origin: [process.env.FRONTEND_URL,process.env.DASHBOARD_URL],
+    methods: ["GET","POST","PUT","DELETE"]
+}))
 
 
 export default app;
