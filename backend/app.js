2da248dbbb752074fc7fb8832482e7050f962e84
MSI <hirun08072002@gmail.com>
2024-10-23 17:49:42 +0530

    add cookieparser and file upload function

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import {config} from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import fileUpload from "express-fileupload";
 
 const app= express();
 config( {path: "./config/config.env"})
@@ -10,6 +12,19 @@ app.use(cors({
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true,
 }));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.use(
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+    })
+);
+
+
 
 
 export default app;
