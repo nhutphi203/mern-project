d706dc872b7b2f31216fd39d83cdb07248a4316a
MSI <hirun08072002@gmail.com>
2024-10-24 07:21:26 +0530

    connect database

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import {config} from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import fileUpload from "express-fileupload";
+import { dbConnection } from "./database/dbConnection.js";
 
 const app= express();
 config( {path: "./config/config.env"})
@@ -24,7 +25,7 @@ app.use(
     })
 );
 
-
+dbConnection();
 
 
 export default app;
--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -3,7 +3,7 @@ import  Mongoose  from "mongoose";
 
 export const dbConnection = () => {
     mongoose.connect(process.env.MONGO_URI,{
-        dbName: "Hospital-Management-System"
+        dbName: "Hospital-Management-System",
     }).then(() => {
         console.log("Connected to database")
     }).catch((err) => {
