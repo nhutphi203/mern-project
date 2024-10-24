4c25a3fa7a594fc63c22b5b7334b499089fc8aa7
MSI <hirun08072002@gmail.com>
2024-10-24 11:55:35 +0530

    const send message complete

--- backend/controller/messageController.js
+++ backend/controller/messageController.js
@@ -1,5 +1,12 @@
 import mongoose  from "mongoose";
 
 export const sendMessage = async(req,res,next) => {
-    
+    const {firstName,lastName,email,phone,message} = req.body;
+    if(!firstName || !lastName || !email || !phone || !message){
+        return res.status(400).json({
+            success: false,
+            message: "please fill full form"
+        });
+    }
+
 }
