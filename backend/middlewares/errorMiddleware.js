0918de00175a75bd1d2c1c04aaa81b5020215498
MSI <hirun08072002@gmail.com>
2024-10-24 17:31:00 +0530

    error code first

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -8,4 +8,9 @@ class ErrorHandler extends Error{
 export const errorMiddleware = (err,req ,res,next) => {
     err.message = err.message || "internal server error"
     err.statusCode= err.statusCode || 500;
+
+    if(err.code === 11000){
+        const message = `Duplicate ${object.keys(err.keyValue)} entered`;
+        err = new ErrorHandler(message,400);
+    }
 }
