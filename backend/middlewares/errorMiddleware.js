f3fbb32737dd3886f93e6328655d9100d26c2b2a
MSI <hirun08072002@gmail.com>
2024-10-24 17:21:54 +0530

    error middleware create

--- backend/middlewares/errorMiddleware.js
+++ backend/middlewares/errorMiddleware.js
@@ -5,3 +5,7 @@ class ErrorHandler extends Error{
     }
 }
 
+export const errorMiddleware = (err,req ,res,next) => {
+    err.message = err.message || "internal server error"
+    err.statusCode= err.statusCode || 500;
+}
