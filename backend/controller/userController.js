38990919b016d890136d53f27069ba26cfcfdce8
MSI <hirun08072002@gmail.com>
2024-10-25 13:34:01 +0530

    complete the if function fully.

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,7 +3,9 @@ import ErrorHandler from "../middlewares/errorMiddleware.js"
 
 export const patientRegister= catchAsyncErrors(async(req,res,next) => {
     const{firstName,lastName,email,phone,password,gender,dob,nic,role} = req.body;
-    if (condition) {
+    if (!firstName||!lastName||!email||!phone||!password||!gender||!dob||!nic||!role) {
+        return next(new ErrorHandler("please fill full form!", 400));
+        
         
     }
 })
