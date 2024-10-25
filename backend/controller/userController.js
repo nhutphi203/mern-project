674d63ed40a7997182ed0cb3a8585db4f1c17823
MSI <hirun08072002@gmail.com>
2024-10-25 11:45:41 +0530

    req body making

---
+++ backend/controller/userController.js
@@ -0,0 +1,6 @@
+import { catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
+import ErrorHandler from "../middlewares/errorMiddleware.js"
+
+export const patientRegister= catchAsyncErrors(async(req,res,next) => {
+    const{firstName,lastName,email,phone,password,gender,dob,nic,role} = req.body;
+})
