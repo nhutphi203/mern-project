bb57ccf27ffd8894986c7182d55059b77b7f7fb0
MSI <hirun08072002@gmail.com>
2024-10-25 14:02:54 +0530

    add more in controller

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,11 +1,14 @@
 import { catchAsyncErrors} from "../middlewares/catchAsyncErrors.js"
 import ErrorHandler from "../middlewares/errorMiddleware.js"
+import {User} from "../models/userScheme.js"; 
 
 export const patientRegister= catchAsyncErrors(async(req,res,next) => {
     const{firstName,lastName,email,phone,password,gender,dob,nic,role} = req.body;
     if (!firstName||!lastName||!email||!phone||!password||!gender||!dob||!nic||!role) {
-        return next(new ErrorHandler("please fill full form!", 400));
-        
-        
+        return next(new ErrorHandler("please fill full form!", 400));   
+    }
+    const user= await User.findOne({email});
+    if(user){
+        return next(new ErrorHandler("user already registered",400))
     }
 })
