a827e8bb2da89ae4402e88869c470df863bd7fce
MSI <hirun08072002@gmail.com>
2024-10-25 07:14:52 +0530

    password hashing continue

--- backend/models/userScheme.js
+++ backend/models/userScheme.js
@@ -74,4 +74,13 @@ if(!this.isModified("password")) {
 this.password = await bcrypt.hash(this.password,10)
 });
 
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword,this.password)
+};
+
+userSchema.methods.generateJsonWebToken =function () {
+    return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY,{
+        expiresIn: process.env.JWT_EXPIRES,
+    });
+}
 export const User = mongoose.model("Message", userSchema);
