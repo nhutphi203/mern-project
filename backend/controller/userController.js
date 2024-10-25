2f2a49855b95e3915cc9476800a92fa32d316491
MSI <hirun08072002@gmail.com>
2024-10-25 13:19:02 +0530

    checking starting

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,4 +3,7 @@ import ErrorHandler from "../middlewares/errorMiddleware.js"
 
 export const patientRegister= catchAsyncErrors(async(req,res,next) => {
     const{firstName,lastName,email,phone,password,gender,dob,nic,role} = req.body;
+    if (condition) {
+        
+    }
 })
