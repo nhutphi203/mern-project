ff2a2cdb46668ed09b911f8894fd4686089c0a6a
MSI <hirun08072002@gmail.com>
2024-10-30 16:13:24 +0530

    user router setup

--- backend/router/userRouter.js
+++ backend/router/userRouter.js
@@ -1,4 +1,9 @@
  import express from 'express';
 import router from './messageRouter';
+import { patientRegister } from '../controller/userController.js';
 
- const router = express.router();
+ const router = express.Router();
+
+ router.post("patient/register" ,patientRegister);
+
+ export default router;
