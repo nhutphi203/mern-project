7330e944f0dfd8ddc6316ab9207f1ada59e3c3ef
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 12:20:12 +0530

    appointmentcontroller

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -2,3 +2,21 @@ import {catchAsyncErrors} from '../middlewares/catchAsyncErrors.js'
 import ErrorHandler from  '../middlewares/errorMiddleware.js'
 import {Appointment} from '../models/appointmentSchema.js';
 import {User} from '../models/userScheme.js'
+
+export const postAppointment = catchAsyncErrors(async(req,res,next) => {
+    const{
+        firstName,
+        lastName,
+        email,
+        phone,
+        nic,
+        dob,
+        gender,
+        appointment_data,
+        department,
+        doctor_firstName,
+        doctor_lastName,
+        hasVisited,
+        address,
+    }= req.body;
+})
