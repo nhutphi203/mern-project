730ed153eff1093399b103d47c28f363ba2f0130
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 16:11:18 +0530

    updateAppointmentStatus

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -100,7 +100,7 @@ export const updateAppointmentStatus = catchAsyncErrors(async(req,res,next) => {
     });
     res.status(200).json({
         success: true,
-        message: "Appointment status updated"
+        message: "Appointment status updated",
         appointment,
     });
 })
