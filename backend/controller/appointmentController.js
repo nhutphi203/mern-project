1094bf5b821be6f3abdde32c8554ed81a532db6b
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 14:33:03 +0530

    fix

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -32,7 +32,6 @@ export const postAppointment = catchAsyncErrors(async(req,res,next) => {
         !department ||
         !doctor_firstName ||
         !doctor_lastName ||
-        !hasVisited ||
         !address
     ) {
         return next(new ErrorHandler("Please fill full form!",400));
