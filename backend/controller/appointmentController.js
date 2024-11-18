b24e798c3b3c9d78905d27c1451587d29ee4d6c5
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 12:14:48 +0530

    importing

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -1,2 +1,3 @@
 import {catchAsyncErrors} from '../middlewares/catchAsyncErrors.js'
 import ErrorHandler from  '../middlewares/errorMiddleware.js'
+import {Appointment} from '../models/appointmentSchema.js';
