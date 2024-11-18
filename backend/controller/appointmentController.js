e6e1039705366bdac7b9c22c7b0225aa3c6886a8
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 12:15:55 +0530

    user

--- backend/controller/appointmentController.js
+++ backend/controller/appointmentController.js
@@ -1,3 +1,4 @@
 import {catchAsyncErrors} from '../middlewares/catchAsyncErrors.js'
 import ErrorHandler from  '../middlewares/errorMiddleware.js'
 import {Appointment} from '../models/appointmentSchema.js';
+import {User} from '../models/userScheme.js'
