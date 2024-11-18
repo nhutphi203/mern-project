2dbbc3bdfcc3022100791c02adf28baca4723dab
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 14:23:17 +0530

    auth

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { postAppointment } from '../controller/appointmentController.js';
+import {isAdminAuthenticated,isPatientAuthenticated} from '../middlewares/auth.js'
 
 const router = express.Router();
 
