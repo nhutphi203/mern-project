777d24336b9227e0daebb2cad81a28ea0f382722
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 14:18:16 +0530

    router setup

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import { postAppointment } from '../controller/appointmentController.js';
 
 const router = express.Router();
+
+router.post("/post",postAppointment)
 
 export default router;
