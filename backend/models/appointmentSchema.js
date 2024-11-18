bb847fb4df1a2dfc96d3b2750996312c8dda9ea4
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 09:51:40 +0530

    appointmentSchema

--- backend/models/appointmentSchema.js
+++ backend/models/appointmentSchema.js
@@ -1,45 +1,50 @@
-import mongoose from 'mongoose';
-import validator from 'validator';
+import mongoose from "mongoose";
+import validator from "validator";
 
-
-const appointmentSchema  = new mongoose.Schema({
-    firstName:{
-        type:String,
-        required: true,
-        minLength: [3, "First name must contain at least 3 characters"]
-    },
-    lastName:{
-        type:String,
-        required: true,
-        minLength: [3, "Last name must contain at least 3 characters"]
-    },
-    email:{
-        type: String,
-        required: true,
-        validate: [validator.isEmail, "Please provide a valid email"]
-    },
-    phone:{
-        type:String,
-        required: true,
-        minLength: [10, "phone number must contain 10"],
-        maxLength: [10, "phone number must contain 10"]
-    },
-    nic:{
-        type:String,
-        required: true,
-        minLength: [12, "nic must contain 12"],
-        maxLength: [12, "nic must contain 12"]  
-    },
-    dob:{
-        type: Date,
-        required: [true,"DOB is required!"],
-    },
-    gender:{
-        type: String,
-        required: true,
-        enum: ["Male","Female"],
-    },
-    
-
-
+const appointmentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+    minLength: [3, "First name must contain at least 3 characters"],
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: [3, "Last name must contain at least 3 characters"],
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: [validator.isEmail, "Please provide a valid email"],
+  },
+  phone: {
+    type: String,
+    required: true,
+    minLength: [10, "phone number must contain 10"],
+    maxLength: [10, "phone number must contain 10"],
+  },
+  nic: {
+    type: String,
+    required: true,
+    minLength: [12, "nic must contain 12"],
+    maxLength: [12, "nic must contain 12"],
+  },
+  dob: {
+    type: Date,
+    required: [true, "DOB is required!"],
+  },
+  gender: {
+    type: String,
+    required: true,
+    enum: ["Male", "Female"],
+  },
+  appointment_date: {
+    type: String,
+    required: true,
+  },
+  department: {
+    type: String,
+    required: true,
+  },
+  
 });
