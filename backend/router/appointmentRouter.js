25c6517868bb0f076262a7a696949d820d70c5bd
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-18 15:32:54 +0530

    router set

--- backend/router/appointmentRouter.js
+++ backend/router/appointmentRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { postAppointment } from '../controller/appointmentController.js';
+import { getAllAppointments, postAppointment } from '../controller/appointmentController.js';
 import {isAdminAuthenticated,isPatientAuthenticated} from '../middlewares/auth.js'
 
 const router = express.Router();
 
 router.post("/post",isPatientAuthenticated,postAppointment)
+router.get("/getall",isAdminAuthenticated,getAllAppointments)
 
 export default router;
