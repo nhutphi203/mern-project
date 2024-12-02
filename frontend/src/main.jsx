2a97c33d501841922fb976792316052df255df73
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-02 09:43:15 +0530

    createcontext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,13 @@
-import { StrictMode } from 'react'
+import { createContext, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+export const Context = createContext({isAuthenticated: false});
 
 const AppWrapper = () =>{
   const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const[user,setUser] = useState({})
+  const[user,setUser] = useState({});
 }
 
 createRoot(document.getElementById('root')).render(
