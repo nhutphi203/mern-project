9b90ea7489ae0bdb8d6195bd1123d3efeec1f914
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-02 09:29:57 +0530

    wrapper

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'
 import './App.css';
 
-const AppWrapper = () =>{
-  const [isAuthenticated] = useState(false);
-  const[] = useState({})
-}
+
 
 const App = () => {
   return (
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
 
+
+const AppWrapper = () =>{
+  const [isAuthenticated] = useState(false);
+  const[] = useState()
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
