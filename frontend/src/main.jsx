25a5d37492a9cf492970983974d382e3ef48eda7
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-12-02 13:54:44 +0530

    main.jsx setup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,10 +8,20 @@ export const Context = createContext({isAuthenticated: false});
 const AppWrapper = () =>{
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const[user,setUser] = useState({});
+
+return(
+  <Context.Provider value={{isAuthenticated,setIsAuthenticated,user,setUser }}>
+    <App />
+  </Context.Provider>
+)
+
+
 }
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper/>
   </StrictMode>,
 )
